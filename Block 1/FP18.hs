6c34d69af7e5b9689609bdbf200b563ab8daa7fa
Rowin Veneman <r.veneman@student.utwente.nl>
2020-05-01 17:57:31 +0200

    bubblesort

--- Block 1/FP18.hs
+++ Block 1/FP18.hs
@@ -1,6 +1,17 @@
+import Data.List
 
-bsort :: Ord a => [a] -> [a]
+bubbleSort :: Ord a => [a] -> [a]
+bubbleSort [] = []
+bubbleSort [x] = [x]
+bubbleSort (x:y:xs)
+    | bsort sort = sort
+    | otherwise = bubbleSort sort
+    where sort = (min x y) : bubbleSort ((max x y):xs)
+
+
+bsort  :: Ord a => [a] -> Bool
+bsort [] = True
+bsort [x] = True
 bsort (x:y:xs)
-    | x > y = bsort (x:xs)
-    | otherwise = x:bsort(y:xs)
-bsort xs = xs
+    | x <= y = bsort (y:xs)
+    | otherwise = False
